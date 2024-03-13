2d18e205a4360b5499a266fdc1bdde983060d8f8
parse5214 <cardanoviette123@gmail.com>
2024-03-13 14:21:03 -0500

    There were some changes

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 function handleClick() {
     alert('Button clicked!');
+
+    var title = document.querySelector('h1');
+
+    title.textContent = 'This is a new title';
+}
+
+function handleSpecialClick() {
+    alert('Special Button was clicked');
 }
 
 var button = document.getElementById('btn');
+var specialButton = document.getElementById('specialBtn');
 
 button.addEventListener('click', handleClick);
+specialButton.addEventListener('click', handleSpecialClick);
