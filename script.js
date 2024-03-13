bf488ff8f3b1c547a5d05b8b44c299a6bcb466b6
4black3white <163334044+4black3white@users.noreply.github.com>
2024-03-13 13:08:15 -0500

    'handleClick' function was modified
    The functionality of the 'handleClick' function was modified so that when the button is pressed the content of the paragraph will be modified.

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function handleClick() {
     alert('Button clicked!');
+
+    var paragraph = document.querySelector('p');
+
+    if (paragraph) {
+        paragraph.textContent = 'Button clicked! Paragraph text changed.'
+    }
 }
 
 var button = document.getElementById('btn');
